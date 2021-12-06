555b0f2ec8b526754cc7e2213158c35db8556e14
cc-stan <stanberends4@gmail.com>
2021-12-06 13:22:12 +0100

    Update main.m

--- Day 06/main.m
+++ Day 06/main.m
@@ -18,10 +18,7 @@ x6(1)=1;
 y=zeros(1,7);
 
 c=0;
-m=zeros(1,7);
-for l=0:numel(m)-1
-    m(l+1)=numel(find(input==l));
-end
+m=hist(input,0:6);
 
 while c<80
     c=c+1;
@@ -32,10 +29,7 @@ while c<80
 
     y=[y(2:end),sum([x6,x8])];
 end
-a1=0;
-for l=1:numel(m)
-    a1=a1+m(l)*y(end-l+1);
-end
+a1=y*m';
 end
 
 %% Part 2
@@ -46,10 +40,7 @@ x6(1)=1;
 y=zeros(1,7);
 
 c=0;
-m=zeros(1,7);
-for l=0:numel(m)-1
-    m(l+1)=numel(find(input==l));
-end
+m=hist(input,0:6);
 
 while c<256
     c=c+1;
@@ -60,8 +51,5 @@ while c<256
 
     y=[y(2:end),sum([x6,x8])];
 end
-a2=0;
-for l=1:numel(m)
-    a2=a2+m(l)*y(end-l+1);
-end
+a2=y*m';
 end
