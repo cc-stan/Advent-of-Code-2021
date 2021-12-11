1b1316b1f5c3962ee2b47c0ac0d740d12a83419e
cc-stan <stanberends4@gmail.com>
2021-12-11 19:52:24 +0100

    Day 10/11

--- Day 10/main.m
+++ Day 10/main.m
@@ -2,20 +2,69 @@
 clear
 close all
 
-fid=fopen('input.txt');
-input=fscanf(fid,'%d');
-fclose(fid);  
+input=fileread('input.txt');
+input=splitlines(input);
 
 %% Print the answers
 fprintf('Answer to part 1 is %d\n',part_1(input))
-%fprintf('Answer to part 2 is %d\n',part_2(input))
+fprintf('Answer to part 2 is %d\n',part_2(input))
 
 %% Part 1
 function a1=part_1(input)
-
+a1=0;
+for i=1:numel(input)
+    while 1
+        num_ch=numel(input{i});
+        input{i}=regexprep(input{i},{'\(\)','\[\]','<>','\{\}'},'');
+        if num_ch==numel(input{i})
+            x=double(input{i}==')')+double(input{i}==']')+double(input{i}=='>')+double(input{i}=='}');
+            x=find(x,1,'first');
+            if ~isempty(x)
+                a1=a1+(input{i}(x)==[')',']','}','>'])*[3;57;1197;25137];
+            end
+            break
+        end
+    end
+end
 end
 
 %% Part 2
 function a2=part_2(input)
+cor_lines=zeros(numel(input),1);
+for i=1:numel(input)
+    while 1
+        num_ch=numel(input{i});
+        input{i}=regexprep(input{i},{'\(\)','\[\]','<>','\{\}'},'');
+        if num_ch==numel(input{i})
+            x=double(input{i}==')')+double(input{i}==']')+double(input{i}=='>')+double(input{i}=='}');
+            x=find(x,1,'first');
+            if ~isempty(x)
+                cor_lines(i)=1;
+            end
+            break
+        end
+    end
+end
 
+input=input(~cor_lines);
+a2=zeros(numel(input),1);
+for i=1:numel(input)
+    while ~isempty(input{i})
+        switch input{i}(end)
+            case '('
+                a2(i)=a2(i)*5+1;
+                input{i}(end)='';
+            case '['
+                a2(i)=a2(i)*5+2;
+                input{i}(end)='';
+            case '{'
+                a2(i)=a2(i)*5+3;
+                input{i}(end)='';
+            case '<'
+                a2(i)=a2(i)*5+4;
+                input{i}(end)='';
+        end
+    end
+end
+a2=median(a2);
 end
--- Day 11/main.m
+++ Day 11/main.m
@@ -2,20 +2,77 @@
 clear
 close all
 
-fid=fopen('input.txt');
-input=fscanf(fid,'%d');
-fclose(fid);  
+input=fileread('input.txt');
+input=strtrim(regexprep(input, '.{1}', '$0 '));
+input=str2num(input);
 
 %% Print the answers
 fprintf('Answer to part 1 is %d\n',part_1(input))
-%fprintf('Answer to part 2 is %d\n',part_2(input))
+fprintf('Answer to part 2 is %d\n',part_2(input))
 
 %% Part 1
 function a1=part_1(input)
+input=padarray(input,[1 1],NaN);
+sz=size(input);
 
+n=[1,1,1;1,0,1;1,1,1];
+a1=0;
+
+for i=1:100
+    flashed=false(sz);
+    input=input+1;
+    while 1
+        flashed_now=input>9;
+        flashed_i=find(flashed_now==1);
+        if isempty(flashed_i)
+            break
+        end
+
+        for j=1:numel(flashed_i)
+            if flashed(flashed_i(j))==0
+                [r,c]=ind2sub(sz,flashed_i(j));
+                input(r-1:r+1,c-1:c+1)=input(r-1:r+1,c-1:c+1)+n;
+                flashed(flashed_i(j))=1;
+                input(r,c)=NaN;
+                a1=a1+1;
+            end
+        end
+    end
+    input(flashed)=0;
+end
 end
 
 %% Part 2
 function a2=part_2(input)
+input=padarray(input,[1 1],NaN);
+sz=size(input);
 
+n=[1,1,1;1,0,1;1,1,1];
+
+for i=1:1000
+    flashed=false(sz);
+    input=input+1;
+    while 1
+        flashed_now=input>9;
+        flashed_i=find(flashed_now==1);
+        if isempty(flashed_i)
+            break
+        end
+
+        for j=1:numel(flashed_i)
+            if flashed(flashed_i(j))==0
+                [r,c]=ind2sub(sz,flashed_i(j));
+                input(r-1:r+1,c-1:c+1)=input(r-1:r+1,c-1:c+1)+n;
+                flashed(flashed_i(j))=1;
+                input(r,c)=NaN;
+            end
+        end
+    end
+    input(flashed)=0;
+    flash_check=sum(flashed,'all')==prod(sz-2);
+    if flash_check==1
+        break
+    end
+end
+a2=i;
 end
