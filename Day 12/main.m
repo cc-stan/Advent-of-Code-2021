3dd1110cc5a761053132e298424db1b20e262cdc
cc-stan <stanberends4@gmail.com>
2021-12-13 23:26:59 +0100

    Day 12/13

--- Day 12/main.m
+++ Day 12/main.m
@@ -2,20 +2,60 @@
 clear
 close all
 
-fid=fopen('input.txt');
-input=fscanf(fid,'%d');
-fclose(fid);  
+input=readmatrix('input.txt', Delimiter='-',OutputType='string');
+
+G=graph(input(:,1),input(:,2));
 
 %% Print the answers
-fprintf('Answer to part 1 is %d\n',part_1(input))
-%fprintf('Answer to part 2 is %d\n',part_2(input))
+fprintf('Answer to part 1 is %d\n',part_1(G,string([]),0,"start"))
+fprintf('Answer to part 2 is %d\n',part_2(G,string([]),0,"start",1))
 
 %% Part 1
-function a1=part_1(input)
-
+function n_paths=part_1(G,vis,n_paths,cur_node)
+if cur_node=="start" && numel(vis)>1
+    return
+end
+vis(end+1)=cur_node;
+if cur_node=="end"
+    n_paths=n_paths+1;
+    %fprintf('Path %d: %s\n',n_paths,strjoin(vis,','))
+    return
+end
+nb=neighbors(G,cur_node);
+for i=1:numel(nb)
+    if nb(i)==upper(nb(i))
+        n_paths=part_1(G,vis,n_paths,nb(i));
+    elseif ~contains(nb(i),vis)
+        n_paths=part_1(G,vis,n_paths,nb(i));
+    end
+end
 end
 
 %% Part 2
-function a2=part_2(input)
+function n_paths=part_2(G,vis,n_paths,cur_node,extra_vis)
 
+if cur_node=="start" && numel(vis)>1
+    return
 end
+
+vis(end+1)=cur_node;
+
+if cur_node=="end"
+    n_paths=n_paths+1;
+    %fprintf('Path %d: %s\n',n_paths,strjoin(vis,','))
+    return
+end
+
+nb=neighbors(G,cur_node);
+for i=1:numel(nb)
+    if nb(i)==upper(nb(i))
+        n_paths=part_2(G,vis,n_paths,nb(i),extra_vis);
+    elseif ~contains(nb(i),vis)
+        n_paths=part_2(G,vis,n_paths,nb(i),extra_vis);
+    elseif sum(count(nb(i),vis)) <= extra_vis
+        n_paths = part_2(G, vis, n_paths, nb(i), 0);
+    end
+end
+end
+
+
--- Day 13/main.m
+++ Day 13/main.m
@@ -2,20 +2,62 @@
 clear
 close all
 
-fid=fopen('input.txt');
-input=fscanf(fid,'%d');
-fclose(fid);  
+input=fileread('input.txt');
+input=regexp(input,'\s\s\s','split');
 
 %% Print the answers
 fprintf('Answer to part 1 is %d\n',part_1(input))
-%fprintf('Answer to part 2 is %d\n',part_2(input))
+fprintf('Answer to part 2 is see plot\n')
+part_2(input)
 
 %% Part 1
 function a1=part_1(input)
+dots=str2num(input{1});
+folds_coord=regexp(splitlines(input{2}),'[0-9]*','match');
+folds_coord=arrayfun(@str2num,string([folds_coord{:}]));
+x_or_y=contains(splitlines(input{2}),'y');
+folds_coord(2,:)=x_or_y(2:end);
 
+paper=zeros(flip(max(dots))+1);
+for i=1:numel(dots(:,1))
+    paper(dots(i,2)+1,dots(i,1)+1)=1;
+end
+
+for i=1
+    if folds_coord(2,i)==1
+        paper=paper+flip(paper,1);
+        paper=paper(1:folds_coord(1,i),:);
+    else
+        paper=paper+flip(paper,2);
+        paper=paper(:,1:folds_coord(1,i));
+    end
+end
+paper=paper>0;
+a1=sum(paper,'all');
 end
 
 %% Part 2
 function a2=part_2(input)
+dots=str2num(input{1});
+folds_coord=regexp(splitlines(input{2}),'[0-9]*','match');
+folds_coord=arrayfun(@str2num,string([folds_coord{:}]));
+x_or_y=contains(splitlines(input{2}),'y');
+folds_coord(2,:)=x_or_y(2:end);
 
+paper=zeros(flip(max(dots))+1);
+for i=1:numel(dots(:,1))
+    paper(dots(i,2)+1,dots(i,1)+1)=1;
+end
+
+for i=1:numel(folds_coord(1,:))
+    if folds_coord(2,i)==1
+        paper=paper+flip(paper,1);
+        paper=paper(1:folds_coord(1,i),:);
+    else
+        paper=paper+flip(paper,2);
+        paper=paper(:,1:folds_coord(1,i));
+    end
+end
+paper=paper>0;
+imshow(~paper)
 end
